d78cbbcad34ba95afb0720eb3e0bfa612b0e4fa5
Lance Delos Reyes <lan_drey19@hotmail.com>
2023-05-21 15:06:49 +1200

    ✨ display currently playing song

--- app/app/practice/page.js
+++ app/app/practice/page.js
@@ -2,8 +2,8 @@
 
 import Link from "next/link";
 import { Howl, Howler } from "howler";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 async function getData() {
   const res = await fetch(process.env.LOCALHOST_BASE_URL + "songs");
   // The return value is *not* serialized
@@ -20,12 +20,13 @@ async function getData() {
 
 export default async function Practice() {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [currentSong, setCurrentSong] = useState(null);
   let currentIndex = 0;
   let fetchedSongs = await getData();
+
   const playSound = async () => {
     fetchedSongs = fetchedSongs.sort(() => Math.random() - 0.5);
     setIsPlaying(true);
-    //console.log(process.env.AUDIO_LINK);
 
     const playNextSong = () => {
       if (currentIndex >= fetchedSongs.length) {
@@ -33,11 +34,13 @@ export default async function Practice() {
         console.log("All songs have been played");
         currentIndex = 0;
         setIsPlaying(false);
+        setCurrentSong(null);
         return;
       }
 
       const { link, duration, offset } = fetchedSongs[currentIndex];
-
+      console.log(fetchedSongs[currentIndex]);
+      setCurrentSong(fetchedSongs[currentIndex]);
       const sound = new Howl({
         src: [link],
         html5: true,
@@ -54,13 +57,10 @@ export default async function Practice() {
           setTimeout(playNextSong, parseInt(process.env.ONE_MILLISECOND, 10)); // Delay before playing the next song (2 seconds = 2000 milliseconds)
         },
       });
-
       sound.play("snippet");
     };
 
     playNextSong(); // Start playing the first song
-
-    //console.log("pressed2");
   };
 
   return (
@@ -69,6 +69,11 @@ export default async function Practice() {
       <button onClick={playSound} disabled={isPlaying}>
         {isPlaying ? "Playing..." : "Play"}
       </button>
+      <h1>
+        {currentSong
+          ? `Currently playing: ${currentSong.title} - ${currentSong.artist}`
+          : "No song playing"}
+      </h1>
       <Link href="/">Home</Link>
     </>
   );
